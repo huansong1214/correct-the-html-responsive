f7616cb3a1b117a87f3d32536b7fccc452d9739d
huansong1214 <huansong1214@gmail.com>
2025-06-19 00:42:06 -0400

    Test CORS behavior for the Quote Card Express Part 4

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-//JavaScript
+async function getRandomImage() {
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.data;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRandomImage();
